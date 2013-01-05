2edf53edf03b1460e624e8da232750783680fab9
Mark McDonnell <mark.mcdx@gmail.com>
2013-01-05 19:21:10 +0000

    Initial design patterns

---
+++ Factory/index.php
@@ -0,0 +1,41 @@
+<?php
+    /**
+     * The Factory design pattern provides a simple interface to acquire a new 
+     * instance of an object while sheltering the calling code from the steps 
+     * to determine which base class is actually instantiated
+     */
+
+    class Automobile
+    {
+        private $vehical_make;
+        private $vehical_model;
+
+        public function __construct($make, $model)
+        {
+            $this->vehical_make = $make;
+            $this->vehical_model = $model;
+        }
+
+        public function getDetails()
+        {
+            return $this->vehical_make . ' | ' . $this->vehical_model;
+        }
+    }
+    
+    class AutomobileFactory
+    {
+        public static function create($make, $model)
+        {
+            return new Automobile($make, $model);
+        }
+    }
+
+    $veyron = AutomobileFactory::create('Bugatti', 'Veyron');
+    
+    echo $veyron->getDetails();
+    
+    /**
+     * This code uses a factory to create the Automobile object. There are two * possible benefits to building your code this way, the first is that if * you need to change, rename, or replace the Automobile class later on   * you can do so and you will only have to modify the code in the factory, * instead of every place in your project that uses the Automobile class. * The second possible benefit is that if creating the object is a        * complicated job you can do all of the work in the factory, instead of  * repeating it every time you want to create a new instance.
+     * 
+     * Using the factory pattern isn’t always necessary (or wise). The example * code used here is so simple that a factory would simply be adding      * unneeded complexity. However if you are making a fairly large or       * complex project you may save yourself a lot of trouble down the road by * using factories.
+     */
---
+++ Observer/index.php
@@ -0,0 +1,87 @@
+<?php
+    /**
+     * The Observer design pattern facilitates the creation of objects that 
+     * watch the state of a targeted functionality that is uncoupled from the 
+     * core object.
+     */
+
+    interface ObserverInterface
+    {
+        public function notify(ObservableAbstract $object);
+    }
+
+    abstract class ObservableAbstract
+    {
+        protected $observers = [];
+
+        public function attachObservers($type, ObserverInterface $observer)
+        {
+            # Create an associative Array with the $type as the key
+            $this->observers[$type][] = $observer;
+        }
+
+        protected function notifyObservers($type)
+        {
+            /**
+             * If there is a type found in the list of observer types then    * loop through the associated observer objects and trigger notices
+             */
+            if (!empty($this->observers[$type])) {
+                # Loop each observer within the specified type
+                foreach ($this->observers[$type] as $observer) {
+                    $observer->notify($this);
+                }
+            }
+        }
+    }
+    
+    class Garden extends ObservableAbstract
+    {
+        public $status = 'unkempt';
+
+        public function weed()
+        {
+            $this->status = 'weeded';
+            $this->notifyObservers('afterweed');
+        }
+
+        public function plants()
+        {
+            $this->status = 'new flowers planted';
+            $this->notifyObservers('afterplants');
+        }
+    }
+    
+    class GardenObserver implements ObserverInterface
+    {
+        public function notify(ObservableAbstract $object)
+        {
+            if ($object->status === 'weeded') {
+                echo 'Now I will drink a beer because my garden is ';
+                echo $object->status;
+                echo '<br><br>';
+            } elseif ($object->status === 'new flowers planted') {
+                echo 'I will finish off tomorrow. At least I got ';
+                echo $object->status;
+            }
+        }
+    }
+    
+    /**
+     * Create a new Garden object 
+     * (which extends from the Abstract class `ObservableAbstract`)
+     */
+    $garden = new Garden();
+
+    /**
+     * Create a new Observer object 
+     * (which implements the Interface `ObservableInterface`)
+     */
+    $gardenObserver = new GardenObserver();
+    
+    # Assign the observer to the garden object for multiple events
+    $garden->attachObservers('afterweed', $gardenObserver);
+    $garden->attachObservers('afterplants', $gardenObserver);
+
+    # Execute a method which causes any assigned observers to be notified
+    $garden->weed();
+    $garden->plants();
